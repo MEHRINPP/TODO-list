6cc9c71839db0067e02f3145fcde350ac226d5ff
mehrin <mehrinpp0@gmail.com>
2025-03-08 12:03:47 +0530

    i add some functionalities

--- src/components/Todoapp/ListTask.js
+++ src/components/Todoapp/ListTask.js
@@ -1,12 +1,44 @@
 import React from 'react'
 
-const ListTask = ({task,index,removeTask}) => {
+const ListTask = ({task, index, removeTask, toggleComplete, editMode, editValue, handleEdit, saveEdit, setEditValue, cancelEdit}) => {
   return (
     <>
-        <div className='list-task'>
+      <div className='list-task'>
+        <input 
+          type='checkbox'
+          checked={task.completed}
+          onChange={() => toggleComplete(index)}
+        />
+        
+        {editMode === index ? (
+          <div className="edit-mode">
+            <input
+              type='text'
+              value={editValue}
+              onChange={(e) => setEditValue(e.target.value)}
+            />
+            <button onClick={() => saveEdit(index)} className='save-btn'>Save</button>
+            <button onClick={cancelEdit} className='cancel-btn'>Cancel</button>
+          </div>
+        ) : (
+          <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
             {task.title}
-            <button onClick={()=>{removeTask(index)}} className='delete-btn'>Delete</button>
+          </span>
+        )}
+
+        <div className="buttons-container">
+          {editMode !== index && (
+            <>
+              <button onClick={() => handleEdit(index, task.title)} className='edit-btn'>
+                Edit
+              </button>
+              <button onClick={() => removeTask(index)} className='delete-btn'>
+                Delete
+              </button>
+            </>
+          )}
         </div>
+      </div>
     </>
   )
 }
--- src/components/Todoapp/Todo.js
+++ src/components/Todoapp/Todo.js
@@ -3,34 +3,77 @@ import "./Todo.css";
 import AddTask from './AddTask';
 import ListTask from './ListTask';
 
+
 const Todo = () => {
     const[tasks,setTasks]=useState([]);
+    const[editMode,setEditMode] = useState(null);
+    const[editValue,setEditValue] = useState("");
+    const [sortOrder,setSortOrder] = useState("incompleteFirst");
     useEffect(()=>{
         document.title=`you have ${tasks.length} pending task(s)`;
     })
 
     const addTask = (title)=>{
-     const newTask=[...tasks,{title}]
+     const newTask=[...tasks,{title,completed:false}]
      setTasks(newTask);
 
     }
 
     const removeTask = (index)=>{
-        const newTask=[...tasks]
-        newTask.splice(index,1);
-        setTasks(newTask);
+     const newTasks = tasks.filter((_, i)=> i !== index);
+     setTasks(newTasks);
     }
+
+    const toggleComplete = (index)=>{
+        const newTasks = [...tasks];
+        newTasks[index].completed = !newTasks[index].completed;
+        setTasks(newTasks);
+    }
+
+    const handleEdit = (index,title)=>{
+        setEditMode(index);
+        setEditValue(title);
+    }
+
+    const saveEdit = (index)=>{
+        const newTasks =[...tasks];
+        newTasks[index].title = editValue;
+        setTasks(newTasks);
+        setEditMode(null);
+    }
+
+    const cancelEdit = ()=>{
+        setEditMode(null);
+    }
+
+    const sortTasks = (tasks) => {
+        if (sortOrder === 'incompleteFirst') {
+          return tasks.sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? 1 : -1));
+        } else if (sortOrder === 'completedFirst') {
+          return tasks.sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? -1 : 1));
+        }
+        return tasks;
+      }
+      
+    const handleSortChange = (order)=>{
+        setSortOrder(order);
+    }
+    const sortedTasks = sortTasks(tasks);
   return (
     <>
         <div className='todo-container'>
             <div className='header'>TODO LIST</div>
+            <div className='sort-options'>
+                <button onClick={()=> handleSortChange('incompleteFirst')} className='sort-btn'>Incomplete First</button>
+                <button onClick={()=> handleSortChange('completedFirst')} className='sort-btn'>Completed First</button>
+            </div>
             <div className='add-task'>
                 <AddTask addTask={addTask}/>
             </div>
             <div className='tasks'>
                 
-                {tasks.map((task,index)=>(
-                    <ListTask task={task} removeTask={removeTask} index={index} key={index} />
+                {sortedTasks.map((task,index)=>(
+                    <ListTask task={task} removeTask={removeTask} toggleComplete={toggleComplete} editMode={editMode} editValue={editValue} handleEdit={handleEdit} saveEdit={saveEdit} cancelEdit={cancelEdit}  setEditValue={setEditValue}  index={index} key={index} />
                 ))}
             </div>
         </div>
